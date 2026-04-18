19ddb20271893e5cf4567d4a89ea8a588f3bb1ad
Maxwell Maxwell <maxwellmaxwell252@gmail.com>
2026-04-18 01:46:37 +0100

    Add health check endpoint to the app

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "Profile API is running" });
+});
 app.use("/api", profileRoutes);
 
 app.use(errorHandler);
