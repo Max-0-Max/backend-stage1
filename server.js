f6d575a5a800d3e5efb10bc0dfa42165943df3c2
Maxwell Maxwell <maxwellmaxwell252@gmail.com>
2026-04-18 01:18:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 import dotenv from "dotenv";
+dotenv.config(); // ← must be FIRST before other imports
+
 import app from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
-
 connectDB();
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
